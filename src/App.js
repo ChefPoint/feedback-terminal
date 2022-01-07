accd8090340d6caab3f6d40c4ce835aed67c31b0
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-01-07 03:13:10 +0000

    Changed "Switch" to "Routes"
    According to: https://stackoverflow.com/questions/63124161/attempted-import-error-switch-is-not-exported-from-react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
 /* * */
 /* IMPORTS */
-import React from "react";
-import { Switch, Route, Redirect } from "react-router";
+import React from 'react';
+import { Routes, Route, Redirect } from 'react-router';
 
-import Err from "./components/Err";
-import FinalThankYou from "./sections/final-thank-you/FinalThankYou";
-import SecondQuestion from "./sections/second-question/SecondQuestion";
-import FirstQuestion from "./sections/first-question/FirstQuestion";
-import SetLocation from "./sections/set-location/SetLocation";
+import Err from './components/Err';
+import FinalThankYou from './sections/final-thank-you/FinalThankYou';
+import SecondQuestion from './sections/second-question/SecondQuestion';
+import FirstQuestion from './sections/first-question/FirstQuestion';
+import SetLocation from './sections/set-location/SetLocation';
 
-import "./App.css";
-import "./styles/elements.css";
-import "./styles/shadows.css";
-import "./styles/animate.css";
-import "./styles/grow.css";
+import './App.css';
+import './styles/elements.css';
+import './styles/shadows.css';
+import './styles/animate.css';
+import './styles/grow.css';
 
 class App extends React.Component {
   render() {
     return (
-      <Switch>
+      <Routes>
         {/* IN STORE FEEDBACK */}
         {/* <Redirect exact from="/POSF/:location/answer" to="/POSF" /> */}
-        <Route path="/:location/error" component={Err} />
-        <Route path="/:location/thank-you" component={FinalThankYou} />
-        <Route path="/:location/second/:id" component={SecondQuestion} />
-        <Route path="/:location" component={FirstQuestion} />
+        <Route path='/:location/error' component={Err} />
+        <Route path='/:location/thank-you' component={FinalThankYou} />
+        <Route path='/:location/second/:id' component={SecondQuestion} />
+        <Route path='/:location' component={FirstQuestion} />
 
         {/* Set Location */}
-        <Route path="/" component={SetLocation} />
-        <Redirect to="/" />
-      </Switch>
+        <Route path='/' component={SetLocation} />
+        <Redirect to='/' />
+      </Routes>
     );
   }
 }
