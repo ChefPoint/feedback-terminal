94afaef7939c5925c7519275db6f5cbebe7a1511
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-01-09 06:07:14 +0000

    Introduced the use of ‹‹useState›› and ‹‹useEffect›› APIs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* * */
 /* IMPORTS */
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router';
 
 import Err from './components/Err';
@@ -9,26 +10,37 @@ import SecondQuestion from './sections/second-question/SecondQuestion';
 import FirstQuestion from './sections/first-question/FirstQuestion';
 import SetLocation from './sections/set-location/SetLocation';
 
+import http from './services/httpService';
+
 import './App.css';
 import './styles/elements.css';
 import './styles/shadows.css';
 import './styles/animate.css';
 import './styles/grow.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <Routes>
-        {/* Set Location */}
-        <Route path='/' element={<SetLocation />} />
-        {/* IN STORE FEEDBACK */}
-        <Route path='/:location' element={<FirstQuestion />} />
-        <Route path='/:location/second' element={<SecondQuestion />} />
-        <Route path='/:location/thank-you' element={<FinalThankYou />} />
-        <Route path='/:location/error' element={<Err />} />
-      </Routes>
-    );
-  }
-}
+const App = () => {
+  const [session, setSession] = useState(null);
+  const [options, setOptions] = useState(null);
+
+  useEffect(() => {
+    getOptions();
+    async function getOptions() {
+      const response = await http.get('/options');
+      setOptions(response.data);
+    }
+  }, []);
+
+  return (
+    <Routes>
+      {/* Set Location */}
+      <Route path='/' element={<SetLocation />} />
+      {/* IN STORE FEEDBACK */}
+      <Route path='/:location' element={<FirstQuestion session={session} options={options} />} />
+      <Route path='/:location/second' element={<SecondQuestion options={options} />} />
+      <Route path='/:location/thank-you' element={<FinalThankYou />} />
+      <Route path='/:location/error' element={<Err />} />
+    </Routes>
+  );
+};
 
 export default App;
