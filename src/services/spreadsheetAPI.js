bc228dc88b56cbbe31aebd7b04473f14452e6e8f
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-01-23 01:45:34 +0000

    Update spreadsheetAPI.js

--- src/services/spreadsheetAPI.js
+++ src/services/spreadsheetAPI.js
@@ -1,14 +1,14 @@
 /* * */
 /* IMPORTS */
 import { GoogleSpreadsheet } from "google-spreadsheet";
-import settings from "../settings/general";
+import settings from "../settings/feedback-session";
 
 const doc = new GoogleSpreadsheet(settings["google-sheets-document-id"]);
 
 async function addNewRow(row) {
   await doc.useServiceAccountAuth({
     client_email: process.env.REACT_APP_GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    private_key: process.env.REACT_APP_GOOGLE_PRIVATE_KEY
+    private_key: process.env.REACT_APP_GOOGLE_PRIVATE_KEY,
   });
   await doc.loadInfo();
   const sheet = doc.sheetsById[0];
